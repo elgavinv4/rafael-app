b8cbad51effdbd12ee4855ce3a4863a51e2e3a63
elgavinv4 <rafaelgavin21@gmail.com>
2025-04-24 11:26:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("FUTSAL")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Latihan futsal agar mudah berkembang dan hebat")
